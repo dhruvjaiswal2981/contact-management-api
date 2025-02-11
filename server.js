16a57fb98afed3bc8231f068c3a6ae903c359e23
Dhruv Jaiswal <dhruvujjain@gmail.com>
2025-02-11 15:27:38 +0530

    first commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use("/", contactRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
